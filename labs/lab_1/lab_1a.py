31343749130d87ca8fa71e7883de8783eb01d96f
sahiavd09 <sahithi.avadhanula@gmail.com>
2026-03-04 15:08:41 -0600

    checking remote change

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -8,7 +8,7 @@ with your name. Then, save the code, add it to the staging area, and commit it t
 def main():
     print("Hello World!")
 
-    name = "sahithi" # TODO: Insert your name between the double quotes
+    name = "sahithi A" # TODO: Insert your name between the double quotes
     introduction = f"Hello, My name is {name} and I am a student in the CSS course." # TODO: Insert your name between the double quotes
     print(f"{name} , Welcome to the CSS course!")
     print(f"Introduction: {introduction} , Welcome to the CSS course!")
